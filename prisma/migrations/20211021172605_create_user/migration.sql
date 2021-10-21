7767ac4b7789164fba3de6eee81073a6e068d528
Pedro Lucas <pedro19792001@gmail.com>
2021-10-21 18:20:02 -0300

    verificando se o usuario já existe e tratanto alguns exceptions

--- prisma/migrations/20211021172605_create_user/migration.sql
+++ prisma/migrations/20211021172605_create_user/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "users" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    "github_id" TEXT NOT NULL,
+    "github_id" INTEGER NOT NULL,
     "avatar_url" TEXT NOT NULL,
     "login" TEXT NOT NULL
 );
--- src/controller/AuthenticateUserController.ts
+++ src/controller/AuthenticateUserController.ts
@@ -5,9 +5,15 @@ class AuthenticateUserController {
     async handle(request: Request, response: Response) {
         const { code } = request.body;
         const service = new AuthenticateUserService();
-        const result = await service.execute(code);
+        try {
+            const result = await service.execute(code);
 
-        return response.json(result);
+            return response.json(result);
+
+        } catch (err) {
+            return response.json({ error: err.message });
+        }
+
     }
 }
 export { AuthenticateUserController }
