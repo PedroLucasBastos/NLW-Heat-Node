edeff9e3ae5cbee4304478cd69b18ed6bb5e8457
Pedro Lucas <pedro19792001@gmail.com>
2021-10-21 13:48:13 -0300

    Inicio do projeto e geração do token

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto feito junto com a Rocketseat.
+O projeto consiste em criar uma aplicação web de chat em tempo real.
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+
+const router = Router();
+router.post("/authenticate", new AuthenticateUserController().handle)
+
+export { router }
