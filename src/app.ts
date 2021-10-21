a61f6034c58dee00b1419579db9c0ec1ee8318ee
Pedro Lucas <pedro19792001@gmail.com>
2021-10-21 14:01:53 -0300

    filtro das informações que chegam do gitHub

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,12 @@ app.use(express.json());
 app.use(router);
 
 app.get("/github", (request, response) => {
-    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`); //pedido de permição para a autenticação na aplicação
 });
 
 app.get("/signin/callback", (request, response) => {
-    const { code } = request.query;
+    const { code } = request.query; // comando responsavel por pegar o codigo do usuario que o github passa.
+
     return response.json(code);
 });
 
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,22 @@
 import axios from "axios";
 
+interface IAccessTokenResponse {
+    access_token: string;
+
+}
+
+interface IUserresponse {
+    avatar_url: string;
+    login: string;
+    id: number;
+    name: string;
+}
+
 class AuthenticateUserService {
     async execute(code: string) {
         const url = "https://github.com/login/oauth/access_token";
 
-        const response = await axios.post(url, null, {
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -12,6 +24,12 @@ class AuthenticateUserService {
             },
             headers: {
                 accept: 'application/json'
+            }
+        });
+
+        const response = await axios.get("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
             }
         });
 
